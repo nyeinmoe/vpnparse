19a3412588e33ca5fc59cb0a9018c9104d2b8547
komoe (Monywa) <nyeinm910@gmail.com>
2026-02-10 00:47:19 +0630

    Update stream.go

--- pkgs/outbound/xray/stream.go
+++ pkgs/outbound/xray/stream.go
@@ -3,8 +3,8 @@ package xray
 import (
 	"strings"
 
-	"github.com/komoe-monywa/uri-to-json/pkgs/parser"
-	"github.com/komoe-monywa/uri-to-json/pkgs/utils"
+	"github.com/komoe-monywa/vpnparse/pkgs/parser"
+	"github.com/komoe-monywa/vpnparse/pkgs/utils"
 	"github.com/gogf/gf/v2/encoding/gjson"
 	"github.com/gogf/gf/v2/util/gconv"
 )
