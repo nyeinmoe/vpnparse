d3f7a7f299b39f3e704f9e39f83da32bf1f6a887
ngwayzinmoe <32437857+ngwayzinmoe@users.noreply.github.com>
2026-02-16 22:10:40 +0630

    Remove SingBox handling from GetOutbound function
    Removed SingBox client type handling and related cases from GetOutbound function.

--- pkgs/outbound/outbound.go
+++ pkgs/outbound/outbound.go
@@ -3,7 +3,6 @@ package outbound
 import (
 	"fmt"
 
-	"github.com/komoe-shwemyae/vpnparse/pkgs/outbound/sing"
 	"github.com/komoe-shwemyae/vpnparse/pkgs/outbound/xray"
 	"github.com/komoe-shwemyae/vpnparse/pkgs/parser"
 	"github.com/komoe-shwemyae/vpnparse/pkgs/utils"
@@ -13,7 +12,6 @@ type ClientType string
 
 const (
 	XrayCore ClientType = "xray"
-	SingBox  ClientType = "sing"
 )
 
 func GetOutbound(clientType ClientType, rawUri string) (result IOutbound) {
@@ -29,34 +27,10 @@ func GetOutbound(clientType ClientType, rawUri string) (result IOutbound) {
 			result = &xray.TrojanOut{RawUri: rawUri}
 		case parser.SchemeSS:
 			result = &xray.ShadowSocksOut{RawUri: rawUri}
-		case parser.SchemeWireguard:
-			result = &xray.WireguardOut{RawUri: rawUri}
-		case parser.SchemeWireguardOld:
-			result = &xray.WireguardOut{RawUri: rawUri}
-		case parser.SchemeHysteria2: // [၁] Xray အတွက် Hysteria2 ကို Register လုပ်ခြင်း
-			result = &xray.Hysteria2Out{RawUri: rawUri}
 		default:
 			fmt.Println("unsupported protocol: ", scheme)
 		}
-	case SingBox:
-		switch scheme {
-		case parser.SchemeVmess:
-			result = &sing.SVmessOut{RawUri: rawUri}
-		case parser.SchemeVless:
-			result = &sing.SVlessOut{RawUri: rawUri}
-		case parser.SchemeTrojan:
-			result = &sing.STrojanOut{RawUri: rawUri}
-		case parser.SchemeSS:
-			result = &sing.SShadowSocksOut{RawUri: rawUri}
-		case parser.SchemeSSR:
-			result = &sing.SShadowSocksROut{RawUri: rawUri}
-		case parser.SchemeWireguard:
-			result = &xray.WireguardOut{RawUri: rawUri}
-		case parser.SchemeWireguardOld:
-			result = &xray.WireguardOut{RawUri: rawUri}
-		default:
-			fmt.Println("unsupported protocol: ", scheme)
-		}
+	
 	default:
 		fmt.Println("unsupported client type")
 	}
