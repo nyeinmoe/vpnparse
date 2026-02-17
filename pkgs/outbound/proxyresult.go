54f4ea12f46c0826746e68c429dde84fc8fbd410
ngwayzinmoe <32437857+ngwayzinmoe@users.noreply.github.com>
2026-02-17 13:29:05 +0630

    Update import paths for parser and utils packages

--- pkgs/outbound/proxyresult.go
+++ pkgs/outbound/proxyresult.go
@@ -6,8 +6,8 @@ import (
 
 	"encoding/json"
 
-	"github.com/komoe-shwemyae/vpnparse/pkgs/parser"
-	"github.com/komoe-shwemyae/vpnparse/pkgs/utils"
+	"github.com/nyeinmoe/vpnparse/pkgs/parser"
+	"github.com/nyeinmoe/vpnparse/pkgs/utils"
 	"github.com/gvcgo/goutils/pkgs/gutils"
 )
 
